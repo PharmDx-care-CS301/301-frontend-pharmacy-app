00feeef96737789be22a28e7395fd99b1ca3f575
targuli1 <umid.targuliyev@mail.utoronto.ca>
2021-03-07 05:44:13 +0400

    pharmacist-login updated
    1. Link to signup added
    2. Link to forgot password added
    3. Modified textfield to hide password

--- src/components/login/pharmacist-login.tsx
+++ src/components/login/pharmacist-login.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import logo from "../../logo.png";
+import {Link} from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import {makeStyles} from "@material-ui/core/styles";
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box'
-
 const useStyles = makeStyles((theme) => ({
   root: {
     "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
@@ -23,6 +23,15 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '39px',
     marginTop: '39px'
   },
+    signup: {
+        backgroundColor: '#ffffff',
+        height: '36px',
+        width: 'auto',
+    },
+    linkStyle: {
+      textDecoration: "none"
+    }
+
 }));
 
 function PharmacistLogin() {
@@ -34,23 +43,41 @@ function PharmacistLogin() {
       <img src={logo} alt="logo" className={classes.logo}/>
         <Box className="input-container" width="337px" height="240px" bgcolor="white" flexDirection="column"
              alignItems="center" display="flex">
-          <TextField id="outlined-basic" label="Email" variant="outlined"
-                     style={{
-                       height: 60,
-                       width: 302,
-                       padding: 0,
-                       paddingBottom: 0,
-                       marginBottom: 15,
-                       marginTop: 15
-                     }}
+          <TextField
+             required
+             id="outlined-basic"
+             name="email"
+             label="Email"
+             type="email"
+             variant="outlined"
+             style={{
+               height: 60,
+               width: 302,
+               padding: 0,
+               paddingBottom: 0,
+               marginBottom: 15,
+               marginTop: 15
+             }}
           />
-          <TextField id="outlined-basic" label="Password" variant="outlined"
-                     style={{
-                       height: 60,
-                       width: 302,
-                       marginBottom: 15
-                     }}
+          <TextField
+              required
+              id="outlined-basic"
+              label="Password"
+              type="password"
+              variant="outlined"
+              style={{
+                   height: 60,
+                   width: 302,
+                   marginBottom: 15
+                 }}
           />
+          <Link to="/" className={classes.linkStyle}>
+              <Button className={classes.signup} fullWidth>Forgot password?</Button>
+          </Link>
+          <Link to="/" className={classes.linkStyle}>
+              <Button className={classes.signup} fullWidth>Don't have an account? SignUp!</Button>
+          </Link>
+
           <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
             Sign In
           </Button>
